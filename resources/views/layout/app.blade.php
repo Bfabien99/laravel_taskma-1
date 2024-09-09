676d7c943b9835efc6a6deed5db92b490c28ad37
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-09-09 14:53:38 +0000

    début du projet

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="">Login</a>
+        <a href="">Register</a>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
